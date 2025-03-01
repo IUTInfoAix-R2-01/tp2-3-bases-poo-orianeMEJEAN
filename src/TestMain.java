e603331cb78ea1d581090d8bf986e75f6f064768
oriane <oriane@oriane-VirtualBox>
2025-03-01 10:12:01 +0100

    Main test de Circle

---
+++ src/TestMain.java
@@ -0,0 +1,19 @@
+
+public class TestMain
+{
+   public static void main(String[] args) 
+   {
+
+      Circle2 c1 = new Circle2(1.1);
+      System.out.println(c1);
+      Circle2 c2 = new Circle2();
+      System.out.println(c2);
+
+      c1.setRadius(2.2);
+      System.out.println(c1);
+      System.out.println("radius is : " + c1.getRadius());
+
+      System.out.printf("area is : %.2f%n", c1.getArea());
+      System.out.printf("circumference is : %.2f%n", c1.getCircumference());
+   }
+}
